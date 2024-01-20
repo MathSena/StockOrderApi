80079add5485c4fc9ee7dcc0f9f4260c6b2e621a
MathSena <MathSena07@hotmail.com>
2024-01-20 21:07:14 +0000

    Initial commit

---
+++ src/main/java/com/mathsena/stockorderapi/StockOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.mathsena.stockorderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockOrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockOrderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mathsena/stockorderapi/StockOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathsena.stockorderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
